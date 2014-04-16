b9f89a7e8f3d196f8774596f89c7c447c687e2e5
EmilyPlatzer <emilyplatzer@gmail.com>
2014-04-16 16:19:47 -0600

    Extract methods with Mike

--- catsup_timer.rb
+++ catsup_timer.rb
@@ -1,70 +1,110 @@
-again = "yes"
-@voice = "Vicki"
-phrase = "Your tomato has exploded. Time is up"
-
 def voice_sample
   `say -v ?`.split(/\n/).map { |l| l[0, l.index('en_')].strip }.each { |v| puts v; %x[say -v \"#{v}\" \"I am #{v}\"]; sleep(0.5) }
-  puts "Please choose a voice."
-  @voice = gets.chomp
 end
 
-while again == "yes"
-  while true
-    puts %Q{Do you want to choose the voice or phrase for timer ending?}
-    puts %Q{Put "voice", "phrase" or "both" to choose both. Anything else for no}
-    choosy = gets.chomp
-    case choosy
-      when "voice"
-        puts %Q{What voice would you like to choose? If you want a list and sample, put "help"}
-        @voice = gets.chomp
-        voice_sample if @voice == "help"
-      when "phrase"
-        puts "What phrase would you like to choose?"
-        phrase = gets.chomp
-      when "both"
-        puts %Q{What voice would you like to choose? If you want a list and sample, put "help"}
-        @voice = gets.chomp
-        voice_sample if @voice == "help"
-        puts "What phrase would you like to choose?"
-        phrase = gets.chomp
+def collect_inputs
+  puts %Q{Do you want to choose the voice for timer ending (y or n)?}
+  choose_voice = (gets.chomp == "y")
+
+  if choose_voice
+    puts %Q{What voice would you like to choose? If you want a list and sample, put "help"}
+    voice = gets.chomp
+    if voice == "help"
+      voice_sample
+      puts "Please choose a voice."
+      voice = gets.chomp
     end
-    puts "Do you want to count in seconds, minutes, hours or days?"
-    units = gets.chomp
-    if units == "seconds" ||
-      units == "minutes" ||
-      units == "hours" ||
-      units == "days"
-      puts "How many #{units}?"
-      amount = gets.chomp.to_i
-    else
-      puts "I don't understand"
-    end
-    break
+  else
+    voice = "Vicki"
   end
-  again = "same"
-  while again == "same"
-    start_time = Time.now
 
-    seconds = amount if units == "seconds"
-    seconds = amount * 60 if units == "minutes"
-    seconds = amount * 60 *60 if units == "hours"
-    seconds = amount * 60 *60 * 24 if units == "days"
+  puts %Q{Do you want to choose the phrase for timer ending (y or n)?}
+  choose_phrase = (gets.chomp == "y")
 
-    while Time.now < start_time + seconds
+  if choose_phrase
+    puts "What phrase would you like to choose?"
+    phrase = gets.chomp
+  else
+    phrase = "Your tomato has exploded. Time is up"
+  end
 
-      sleep 1
-      time_left = Time.new(1974)
-      time_left_seconds = seconds - (Time.now - start_time)
-      time_left += time_left_seconds
+  puts "Do you want to count in seconds, minutes, hours or days?"
+  units = gets.chomp
+  if units == "seconds" ||
+    units == "minutes" ||
+    units == "hours" ||
+    units == "days"
+    puts "How many #{units}?"
+    amount = gets.chomp.to_i
+  else
+    puts "I don't understand the units #{units}, please try run_again."
+    exit(1)
+  end
 
-      print "\r"
-      print time_left.strftime("%H:%M:%S")
-    end
-    print "\r"
-    puts "00:00:00"
-    puts phrase
-    system(%Q{say -v #{@voice} #{phrase}})
-    puts %Q{Again? Put "same" for same as before, or "yes" to reenter parameters, anything else for exit.}
-    again = gets.chomp
+  {:voice => voice, :phrase => phrase, :units => units, :amount => amount}
+end
+
+def calculate_seconds(amount, units)
+  if units == "seconds"
+    amount
+  elsif units == "minutes"
+    amount * 60
+  elsif units == "hours"
+    amount * 60 *60
+  else
+    amount * 60 *60 * 24
+  end
+end
+
+def run_again?
+  puts %Q{Again? y or n}
+  gets.chomp == 'y'
+end
+
+def same_inputs?
+  puts %Q{Use the same inputs voice, phrase and time? y or n}
+  gets.chomp == 'y'
+end
+
+def output_time_left(seconds, start_time)
+  time_left = Time.new(1974)
+  time_left_seconds = seconds - (Time.now - start_time)
+  time_left += time_left_seconds
+  print "\r"
+  print time_left.strftime("%H:%M:%S")
+end
+
+def output_ending(voice, phrase)
+  print "\r"
+  puts "00:00:00"
+  puts phrase
+  system(%Q{say -v #{voice} #{phrase}})
+end
+
+run_again = true
+same_inputs = false
+
+while run_again
+  user_input = nil if !user_input
+
+  if !same_inputs
+    user_input = collect_inputs
+  end
+
+  start_time = Time.now
+
+  seconds = calculate_seconds(user_input[:amount], user_input[:units])
+
+  while Time.now < start_time + seconds
+    sleep 1
+    output_time_left(seconds, start_time)
+  end
+
+  output_ending(user_input[:voice], user_input[:phrase])
+
+  run_again = run_again?
+
+  if run_again
+    same_inputs = same_inputs?
   end
 end
