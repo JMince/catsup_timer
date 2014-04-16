8533530f674bd4b11d88219bca4bcab1157eccc8
EmilyPlatzer <emilyplatzer@gmail.com>
2014-04-16 13:39:17 -0600

    Bug squashing? No rerun now means no.

--- catsup_timer.rb
+++ catsup_timer.rb
@@ -7,38 +7,37 @@ while again == "yes"
       units == "minutes" ||
       units == "hours" ||
       units == "days"
+      puts "How many #{units}?"
+      amount = gets.chomp.to_i
       break
     else
       puts "I don't understand"
     end
   end
-  while true
-    puts "How many #{units}?"
-    amount = gets.chomp.to_i
-    again = "same"
-    while again == "same"
-      start_time = Time.now
+  again = "same"
+  while again == "same"
+    start_time = Time.now
 
-      seconds = amount if units == "seconds"
-      seconds = amount * 60 if units == "minutes"
-      seconds = amount * 60 *60 if units == "hours"
-      seconds = amount * 60 *60 * 24 if units == "days"
+    seconds = amount if units == "seconds"
+    seconds = amount * 60 if units == "minutes"
+    seconds = amount * 60 *60 if units == "hours"
+    seconds = amount * 60 *60 * 24 if units == "days"
 
-      while Time.now < start_time + seconds
+    while Time.now < start_time + seconds
 
-        sleep 1
-        time_left = Time.new(1974)
-        time_left_seconds = seconds - (Time.now - start_time)
-        time_left += time_left_seconds
+      sleep 1
+      time_left = Time.new(1974)
+      time_left_seconds = seconds - (Time.now - start_time)
+      time_left += time_left_seconds
 
-        print "\r"
-        print time_left.strftime("%H:%M:%S")
-      end
       print "\r"
-      puts "Your tomato has exploded. Time is up"
-      system(%Q{say -v "Vick" "Your tomato has exploded. Time is up"})
-      puts %Q{Again? Put "same" for same as before, or "yes" to reenter parameters, anything else for exit.}
-      again = gets.chomp
+      print time_left.strftime("%H:%M:%S")
     end
+    print "\r"
+    puts "00:00:00"
+    puts "Your tomato has exploded. Time is up"
+    system(%Q{say -v "Vick" "Your tomato has exploded. Time is up"})
+    puts %Q{Again? Put "same" for same as before, or "yes" to reenter parameters, anything else for exit.}
+    again = gets.chomp
   end
 end
